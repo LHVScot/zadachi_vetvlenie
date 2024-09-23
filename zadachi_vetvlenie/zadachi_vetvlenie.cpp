26caf9bbc470ed0c0086ea72c4a353a5f25d2e30
LHVSkot <lhvskot4@gmiail.com>
2024-09-23 13:58:46 +0900

    Добавьте файлы проекта.

---
+++ zadachi_vetvlenie/zadachi_vetvlenie.cpp
@@ -0,0 +1,64 @@
+﻿// zadachi_vetvlenie.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{//33в
+    setlocale(LC_ALL, "Ru");
+    //double x, y;
+    //cout << "введите х и y: "<< endl;
+    //cin >> x >> y;//ввод двух значений
+    //if (x > y)//сравнение x и y
+    //{
+    //    cout << "минимальное число y: " << y;
+    //}
+    //else if (x < y)//сравнение x и y
+    //{
+    //    cout << "минимальное число x: " << x;
+    //}
+    //else {
+    //    cout << "числа равны!";
+    //}
+    //47
+    /*int x, y, z;
+    cout << "введите x: " << endl;
+    cin >> x;
+    cout << "введите y: " << endl;
+    cin >> y;
+    cout << "введите z: " << endl;
+    cin >> z;
+    if (x != 0 || y != 0|| z != 0)
+    {
+        cout << "треугольник существет" << endl;
+        if (x < 90 || z < 90 || y < 90) {
+            cout << "треугольник остроугольный" << endl;
+        }
+        else {
+            cout << "треугольник не остроугольный" << endl;
+        }
+    }
+    else {
+        cout << "треугольник не существет" << endl;
+    }*/
+    //60г
+    double x, y;
+    string D;
+    float res;
+    cout << "введите x: " << endl;
+    cin >> x;
+    cout << "введите y: " << endl;
+    cin >> y;
+    cout << "принадлежит ли D к x и y?  " << endl;
+    cin >> D;
+    if (D == "да") {
+        res = (x * x) - 1;
+        cout << "в таком случе U = " << res << endl;
+    }
+    else {
+        res = sqrt(abs(x)) - 1;
+        cout << "в таком случе U = " << res << endl;
+    }
+}
+
